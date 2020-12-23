f4e567f94052ba4aa2c222d2cda7407414044433
quqixun <quqixun@gmail.com>
2020-12-23 22:33:06 +0800

    start GAT

--- 2.GAT/main.py
+++ 2.GAT/main.py
@@ -1,5 +1,5 @@
 from script.cora import CoraData
-from script.pipeline import Pipeline
+# from script.pipeline import Pipeline
 from script.prepare import prepare_data
 
 
@@ -7,23 +7,25 @@ if __name__ == '__main__':
 
     # 数据下载与预处理
     cora = CoraData(output_dir='./data/cora', rebuild=False)
-    dataset = prepare_data(cora)
+    dataset = prepare_data(cora, sparse=False)
 
-    # # 训练参数
-    # params = {
-    #     'model': {
-    #         'input_dim': 1433,              # 节点特征维度
-    #         'hidden_dims': [128, 7],        # 隐层输出特征维度
-    #         'num_neighbors_list': [10, 10]  # 没接采样邻居的节点数
-    #     },
-    #     'hyper': {
-    #         'lr': 3e-3,                # 优化器初始学习率
-    #         'epochs': 10,              # 训练轮次
-    #         'batch_size': 16,          # 批数据大小
-    #         'weight_decay': 5e-4,      # 优化器权重衰减
-    #         'num_batch_per_epoch': 20  # 每个epoch循环的批次数
-    #     }
-    # }
+    # 训练参数
+    params = {
+        'model': {
+            'input_dim': 1433,  # 节点特征维度
+            'hidden_dim': 8,    # 隐层输出特征维度
+            'output_dim': 7,    # 节点类别个数
+            'num_heads': 8,     # 多头注意力个数
+            'dropout': 0.6,     # dropout比例
+            'alpha': 0.2        # LeakyReLU斜率
+        },
+        'hyper': {
+            'lr': 3e-3,           # 优化器初始学习率
+            'epochs': 10,         # 训练轮次
+            'patience': 100,      # 早停轮次
+            'weight_decay': 5e-4  # 优化器权重衰减
+        }
+    }
 
     # # 训练模型
     # pipeline = Pipeline(**params)
