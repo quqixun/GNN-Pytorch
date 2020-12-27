6582b522ded304caf5efc60e301f92f0d8ee10f2
quqixun <quqixun@gmail.com>
2020-12-27 23:58:06 +0800

    adopt script for pubmed and citeseer

--- 0.GCN/main.py
+++ 0.GCN/main.py
@@ -1,35 +1,36 @@
-from script.cora import CoraData
+from script.dataset import Dataset
+from script.prepare import prepare
 from script.pipeline import Pipeline
-from script.prepare import prepare_data
 
 
 if __name__ == '__main__':
 
     # 数据下载与预处理
-    cora = CoraData(output_dir='./data/cora', rebuild=False)
-    dataset = prepare_data(cora)
+    # data可选列表['cora', 'pubmed', 'citeseer']
+    dataset = Dataset(data='citeseer', dataset_dir='../dataset')
+    # dataset = prepare(dataset)
 
-    # 训练参数
-    params = {
-        'random_state': 42,       # 随机种子
-        'model': {
-            'input_dim': 1433,    # 节点特征维度
-            'output_dim': 7,      # 节点类别数
-            'hidden_dim': 16,     # 隐层输出特征维度
-            'use_bias': True      # 是否使用偏置
-        },
-        'hyper': {
-            'lr': 1e-2,           # 优化器初始学习率
-            'epochs': 200,        # 训练轮次
-            'weight_decay': 5e-4  # 优化器权重衰减
-        }
-    }
+    # # 训练参数
+    # params = {
+    #     'random_state': 42,       # 随机种子
+    #     'model': {
+    #         'input_dim': 1433,    # 节点特征维度
+    #         'output_dim': 7,      # 节点类别数
+    #         'hidden_dim': 16,     # 隐层输出特征维度
+    #         'use_bias': True      # 是否使用偏置
+    #     },
+    #     'hyper': {
+    #         'lr': 1e-2,           # 优化器初始学习率
+    #         'epochs': 100,        # 训练轮次
+    #         'weight_decay': 5e-4  # 优化器权重衰减
+    #     }
+    # }
 
-    # 训练模型
-    pipeline = Pipeline(**params)
-    pipeline.train(dataset)
+    # # 训练模型
+    # pipeline = Pipeline(**params)
+    # pipeline.train(dataset)
 
-    # 测试集准确率
-    test_acc = pipeline.predict(dataset, 'test')
-    print('Test Accuracy: {:.6f}'.format(test_acc))
-    # Test Accuracy: 0.819
+    # # 测试集准确率
+    # test_acc = pipeline.predict(dataset, 'test')
+    # print('Test Accuracy: {:.6f}'.format(test_acc))
+    # # Test Accuracy: 0.819
--- 0.GCN/script/prepare.py
+++ 0.GCN/script/prepare.py
@@ -1,4 +1,4 @@
-"""Cora数据预处理
+"""dataset数据预处理
 
     1. 归一化节点特征
     2. 将节点划分为训练集、验证集和测试集
@@ -39,8 +39,8 @@ def normalize_adjacency(adjacency):
     return norm_adjacency
 
 
-def prepare_data(cora):
-    """Cora数据预处理
+def prepare(dataset):
+    """数据预处理
 
         1. 归一化节点特征
         2. 将节点划分为训练集、验证集和测试集
@@ -49,13 +49,13 @@ def prepare_data(cora):
 
         Input:
         ------
-        cora: Data, 包含的元素为:
-              X: numpy array, 节点特征
-              y: numpy array, 节点类别标签
-              adjacency: sparse numpy array, 邻接矩阵
-              test_mask: numpy array, 测试集样本mask
-              train_mask: numpy array, 训练集样本mask
-              valid_mask: numpy array, 验证集样本mask
+        dataset: Data, 包含的元素为:
+                 X: numpy array, 节点特征
+                 y: numpy array, 节点类别标签
+                 adjacency: sparse numpy array, 邻接矩阵
+                 test_mask: numpy array, 测试集样本mask
+                 train_mask: numpy array, 训练集样本mask
+                 valid_mask: numpy array, 验证集样本mask
 
         Output:
         -------
@@ -70,35 +70,36 @@ def prepare_data(cora):
     """
 
     # 节点特征归一化
-    X = cora.data.X / cora.data.X.sum(1, keepdims=True)
+    X = dataset.data.X / dataset.data.X.sum(1, keepdims=True)
     X = torch.from_numpy(X)
 
     # 节点标签
-    y = torch.from_numpy(cora.data.y)
+    y = torch.from_numpy(dataset.data.y)
 
     # 节点划分
-    test_mask = torch.from_numpy(cora.data.test_mask)
-    train_mask = torch.from_numpy(cora.data.train_mask)
-    valid_mask = torch.from_numpy(cora.data.valid_mask)
+    test_mask = torch.from_numpy(dataset.data.test_mask)
+    train_mask = torch.from_numpy(dataset.data.train_mask)
+    valid_mask = torch.from_numpy(dataset.data.valid_mask)
 
     # 邻接矩阵正则化
-    norm_adjacency = normalize_adjacency(cora.data.adjacency)
+    norm_adjacency = normalize_adjacency(dataset.data.adjacency)
     indices = np.asarray([norm_adjacency.row, norm_adjacency.col])
     indices = torch.from_numpy(indices.astype(int)).long()
     values = torch.from_numpy(norm_adjacency.data.astype(np.float32))
-    adjacency = torch.sparse.FloatTensor(indices, values, (2708, 2708))
+    adjacency = torch.sparse.FloatTensor(indices, values, (len(X), len(X)))
 
-    # 数据加载至的设备
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # # 数据加载至的设备
+    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    # 合并数据
-    dataset = Data(
-        X=X.to(device),
-        y=y.to(device),
-        adjacency=adjacency.to(device),
-        test_mask=test_mask.to(device),
-        train_mask=train_mask.to(device),
-        valid_mask=valid_mask.to(device)
-    )
+    # # 合并数据
+    # dataset = Data(
+    #     X=X.to(device),
+    #     y=y.to(device),
+    #     adjacency=adjacency.to(device),
+    #     test_mask=test_mask.to(device),
+    #     train_mask=train_mask.to(device),
+    #     valid_mask=valid_mask.to(device)
+    # )
 
-    return dataset
+    # return dataset
+    return None
